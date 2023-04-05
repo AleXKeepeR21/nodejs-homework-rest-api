fb0e167f45717e86702c122018cc22ed9d220212
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-04-05 21:11:50 +0300

    add verify

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 const path = require("path");
 const fs = require("fs/promises");
 const gravatar = require("gravatar");
+const uuid = require("uuid").v4;
 
 const Users = require("../models/usersModel");
-
+const { sendEmail } = require("../services/sendEmail");
 const { registerValidator, loginValidator } = require("../validator/validator");
 
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
@@ -33,21 +34,31 @@ const register = async (req, res, next) => {
       });
     }
 
-    // const avatarURL = gravatar.url(email);
+    const verificationToken = uuid();
+
     const avatarURL = gravatar.url(email, { d: "wavatar" });
 
     const newUser = await Users.create({
       ...req.body,
       avatarURL,
+      verificationToken,
     });
     newUser.password = undefined;
 
     const token = signToken(newUser.id);
 
+    const mail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}"> Click to verify your email </a>`,
+    };
+    await sendEmail(mail);
+
     res.status(201).json({
       user: newUser,
       token,
       avatarURL,
+      verificationToken,
     });
   } catch (error) {
     console.log(error.message);
@@ -66,8 +77,10 @@ const login = async (req, res) => {
     }
 
     const user = await Users.findOne({ email }).select("+password");
-    if (!user) {
-      res.status(401).json({ message: "Email or password is wrong" });
+    if (!user || !user.verify) {
+      res
+        .status(401)
+        .json({ message: "Email is wrong or not verify or password is wrong" });
     }
 
     const passwordIsValid = await user.checkPassword(password, user.password);
@@ -127,10 +140,71 @@ const updateUser = async (req, res) => {
   }
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+  try {
+    const user = await Users.findOne({ verificationToken });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    user.verificationToken = null;
+    user.verify = true;
+
+    await Users.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+    res.status(200).json({ message: "Verification successful" });
+
+    // await user.save();
+
+    // return res.status(200).json({ message: "Verification successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const verifyEmailRepeat = async (req, res) => {
+  const { email } = req.body;
+  const user = await Users.findOne({ email });
+
+  const { verificationToken } = user;
+
+  if (!email) {
+    return res.status(400).json({
+      code: 400,
+      message: "Missing required field email",
+    });
+  }
+
+  if (user.verify) {
+    return res.status(400).json({
+      code: 400,
+      message: "Verification has already been passed",
+    });
+  }
+
+  const mail = {
+    to: email,
+    subject: "Email submission",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Submit email</a>`,
+  };
+
+  await sendEmail(mail);
+
+  res.json({
+    status: "Ok",
+    code: 200,
+    message: "Verification email sent",
+  });
+};
+
 module.exports = {
   register,
   login,
   current,
   logout,
   updateUser,
+  verifyEmail,
+  verifyEmailRepeat,
 };
